9fc7c1219cd0973ae12d10f90e3fe672329eaea8
Raúl González <rafex.dev@gmail.com>
2019-05-08 10:36:30 -0500

    refactor

---
+++ CLI/src/main/java/mx/rafex/noobz/cli/sesiones/SesionCli.java
@@ -0,0 +1,11 @@
+package mx.rafex.noobz.cli.sesiones;
+
+import mx.rafex.noobz.cli.dtos.usuarios.UsuarioCliDto;
+
+public class SesionCli {
+
+    public Boolean iniciarSesion(final UsuarioCliDto usuario) {
+
+        return true;
+    }
+}
---
+++ DTO/src/main/java/mx/rafex/noobz/cli/dtos/personas/PersonaCliDto.java
@@ -0,0 +1,33 @@
+package mx.rafex.noobz.cli.dtos.personas;
+
+public class PersonaCliDto {
+
+    private String nombre;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+}
